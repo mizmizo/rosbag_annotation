6e0cd24b83e4260e13af7e9a708daba8d16abd90
mizmizo <mizohana@jsk.imi.i.u-tokyo.ac.jp>
2017-07-29 16:51:26 +0900

    add erase annotation mode

--- image_annotation.py
+++ image_annotation.py
@@ -18,6 +18,7 @@ from cv_bridge import CvBridge
 image_topic_ = '/stereo/left/image_rect_color' ## topics of interest
 save_directory = '/home/mizmizo/tmp_data/conveni_data/train_data2'
 w_counter = 0 ## Start saving label from this number
+keep_label = True ## true: reuse labels of previous image on next image
 
 ref_pt = []
 image = None
@@ -38,15 +39,19 @@ rect_vec = []
 ## <label_string 0.0 0 0.0 x_min y_min x_max y_max 0.0 0.0 0.0 0.0 0.0 0.0 0.0>
 
 def usage():
-    print("\n\n1. Input class ID. Type 'l' for check class list.")
-    print("2. Click and drag to rectify object area.")
-    print("3. Type 'a' for accept label, or type 'e' for retry selecting area")
-    print("4. Type 'a' for adding a label, 'n' for going to next image, or 'q' for quiting annotation")
-    print("5. Input ID for adding next label, or type n for going to next image")
-    print("\nOther commands(only enable in ID select phase):")
-    print("  r: reset and erase the label file for current image")
+    print("\n\n1-1. Input class ID.")
+    print("1-2. Click and drag to rectify object area.")
+    print("1-3. Type 'o' for accept label, or type 'r' for retry selecting area")
+    print("2. Type 's' for going to next image. selected labels will be saved unless it's empty.")
+    print("\nSpecial commands(always available):")
+    print("  l: show class list")
     print("  h: show this help")
-    print("  s: skip the image\n")
+    print("  c: show selected label")
+    print("  q: exit this program")
+    print("  a: enter add annotation mode")
+    print("  e: enter erase annotation mode")
+    print("  m: enter modify annotation mode")
+    print("  s: go to next image")
 
 def show_dict(dict):
     print("Class list:")
@@ -68,6 +73,7 @@ def draw_rect():
 # return True if state is changed
 def check_input(key_val):
     global state
+    print("key input: {0}".format(key_val))
     if key_val == 'l':
         show_dict(id_dict)
         return False
@@ -83,11 +89,14 @@ def check_input(key_val):
     elif key_val == 'a':
         state = "waitaddanno"
         return True
-    elif key_val == 's':
-        state = "norun"
+    elif key_val == 'e':
+        state = "eraseanno"
         return True
     elif key_val == 'm':
         state = "waitmodanno"
+        return True
+    elif key_val == 's':
+        state = "norun"
         return True
     else:
         return False
@@ -104,9 +113,37 @@ def finish_imageproc():
                              float(rect[1][0]), float(rect[1][1])))
         text_file.close()
         cv2.imwrite(save_directory + '/images/' + suffix_name + '.jpg', save_image)
-        class_vec = []
-        rect_vec = []
+        if not keep_label:
+            class_vec = []
+            rect_vec = []
         w_counter += 1
+
+## return  clicked rect_id and (0: center | 1:left-top | 2:left-bottom | 3:right-bottom | 4:right-top | 5:none)
+def click_rect_check(pt):
+    vertex_thre = 5 # threthold for detecting vertex click in px
+    found_center_click = []
+    found_vertex_click = []
+    for i, rect in enumerate(rect_vec):
+        if rect[0][0] < pt[0] < rect[1][0] and rect[0][1] < pt[1] < rect[1][1]:
+            found_center_click.append(i)
+        if abs(rect[0][0] - pt[0]) < vertex_thre:
+            if abs(rect[0][1] - pt[1]) < vertex_thre:
+                found_vertex_click.append((i, 1, np.sqrt(np.square((rect[0][0] - pt[0])) + np.square((rect[0][1] - pt[1]))))) # (id, vertex, dist)
+            elif abs(rect[1][1] - pt[1]) < vertex_thre:
+                found_vertex_click.append((i, 2, np.sqrt(np.square((rect[0][0] - pt[0])) + np.square((rect[1][1] - pt[1]))))) # (id, vertex, dist)
+        elif abs(rect[1][0] - pt[0]) < vertex_thre:
+            if abs(rect[1][1] - pt[1]) < vertex_thre:
+                found_vertex_click.append((i, 3, np.sqrt(np.square((rect[1][0] - pt[0])) + np.square((rect[1][1] - pt[1]))))) # (id, vertex, dist)
+            elif abs(rect[0][1] - pt[1]) < vertex_thre:
+                found_vertex_click.append((i, 4, np.sqrt(np.square((rect[1][0] - pt[0])) + np.square((rect[0][1] - pt[1]))))) # (id, vertex, dist)
+
+    if len(found_vertex_click) != 0:
+        found_vertex_click.sort(key = lambda x:x[2])
+        return found_vertex_click[0][0], found_vertex_click[0][1]
+    elif len(found_center_click) != 0:
+        return found_center_click[0], 0
+    else:
+        return 0, 5
 
 
 def click_annotate_rect(event, x, y, flags, param):
@@ -124,7 +161,7 @@ def click_annotate_rect(event, x, y, flags, param):
                        (max(ref_pt[0][0], ref_pt[1][0]), max(ref_pt[0][1], ref_pt[1][1]))]
             ref_pt = swap_pt
             state = "waitaddanno"
-            print("OK? if ok type 'o' or 'r'")
+            print("OK? if ok type 'o' for ok,  or 'r' for reject")
             sys.stdout.flush()
             operating = False
             return
@@ -136,6 +173,20 @@ def click_annotate_rect(event, x, y, flags, param):
             cv2.rectangle(tmp_image, swap_pt[0], swap_pt[1], color_list[int(selected_id)], 2)
             cv2.imshow(window_name, tmp_image)
             cv2.waitKey(1)
+
+    ## Erase annotation
+    if state == "eraseanno":
+        if event == cv2.EVENT_LBUTTONDOWN and len(class_vec) != 0:
+            erase_pt = (x, y)
+            click_id, click_state = click_rect_check(erase_pt)
+            print("click at {0}, {1} : on rect {2}: state {3}".format(x, y, click_id, click_state))
+            if click_state != 5: # selected
+                del class_vec[click_id]
+                del rect_vec[click_id]
+                draw_rect()
+                cv2.imshow(window_name, rected_image)
+        return
+
     ## Modify annotation
     elif state == "modanno":
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -169,7 +220,7 @@ def read_rosbag(bag_path, class_path):
     global id_dict, w_counter
     bag = rosbag.Bag(bag_path, 'r')
     bridge = CvBridge()
-
+    
     ## register class list
     for line in open(class_path, 'r'):
         line = line.rstrip("\n")
@@ -211,7 +262,8 @@ def read_rosbag(bag_path, class_path):
             for i in xrange(0, image.shape[1], stride):
                 cv2.circle(image, (i, j), 2, (0, 255, 0), -1)
         rected_image = image.copy()
-        cv2.imshow(window_name, image)
+        draw_rect()
+        cv2.imshow(window_name, rected_image)
         saved = False
 
         ### choose operation
@@ -232,9 +284,10 @@ def read_rosbag(bag_path, class_path):
             continue
 
         ### operate on image
-        while True:
+        while state != "norun" and "exit":
             global selected_id
 
+            ### Add annotation
             if state == "waitaddanno":
                 print("Enter id num : ", end="")
                 sys.stdout.flush()
@@ -243,13 +296,12 @@ def read_rosbag(bag_path, class_path):
                 while True:
                     key = cv2.waitKey(0) & 0xFF
                     val = chr(key)
-                    if check_input(val):
+                    if check_input(val) and state != "waitaddanno":
                         break
                     elif val in id_dict.keys():
                         print(id_dict[val])
                         selected_id = val
                         break
-
                 if state == "waitaddanno":
                     print("draw rect")
                     sys.stdout.flush()
@@ -258,15 +310,16 @@ def read_rosbag(bag_path, class_path):
                     while True:
                         key = cv2.waitKey(0) & 0xFF
                         val = chr(key)
-                        if check_input(val) and state != "waitaddanno":
-                            break
-                        if val == 'o':
+                        if check_input(val):
+                            print("You can't change mode now! answer 'o' or 'r' first")
+                        elif val == 'o':
                             print("add label")
                             sys.stdout.flush()
                             class_vec.append(id_dict[selected_id])
                             rect_vec.append(ref_pt)
                             draw_rect()
                             cv2.imshow(window_name, rected_image)
+                            state != "waitaddanno"
                             break
                         elif val == 'r':
                             print("Retry")
@@ -277,21 +330,29 @@ def read_rosbag(bag_path, class_path):
                         else:
                             print("answer in 'o' or 'r'")
 
-                if state != "waitaddanno":
-                    break
+            ### Erase annotation
+            if state == "eraseanno":
+                print("Click rectangle to erase.")
+                sys.stdout.flush()
+                val = ''
+                while True:
+                    key = cv2.waitKey(0) & 0xFF
+                    val = chr(key)
+                    if check_input(val) and state != "eraseanno":
+                        break
 
-        if state == "norun":
-            finish_imageproc()
-            continue
-        elif state == "exit":
-            continue
+            if state == "norun":
+                finish_imageproc()
+                continue
+            elif state == "exit":
+                continue
 
     bag.close()
 
 
 def main(argv):
     if len(argv) < 3:
-        print('provide path to rosbag and class list')
+        print('Usage: image_annotation.py <rosbag> <classlist>')
         return
 
     print('\033[33mchecking directory\033[0m')
